b5759fb20e8a366f340e25fe437fc6272c350017
audioprogblog <43120528+audioprogblog@users.noreply.github.com>
2019-10-04 01:47:38 +0100

    Add files via upload

---
+++ StartSequence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSequence : MonoBehaviour
+{
+    public float playerY, playerYStart;
+    public GameObject player;
+    public bool hasStarted;
+    public string firstEvent;
+
+    void Start()
+    {
+        hasStarted = false;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        //Track Y value of headset
+        if (hasStarted == false)
+        {
+            playerY = player.transform.position.y;
+        }
+
+        //If Y value < half of it's initial value game will progress
+        if (playerY <= (playerYStart / 2) && hasStarted == false)
+        {
+            AkSoundEngine.PostEvent(firstEvent, gameObject);
+            hasStarted = true;
+        }
+    }
+}
